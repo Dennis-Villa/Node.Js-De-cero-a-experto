91e7c4422d83346900109268801bc1c79a54fb1f
Dennis-Villa <dennisvillavicencioq@gmail.com>
2024-09-25 15:29:54 +0100

    Sección 6 terminada

---
+++ 04-Multiplication/src/app.old.ts
@@ -0,0 +1,38 @@
+import { writeFileSync, mkdirSync } from 'fs';
+import { yarg } from './config/plugins/yargs.plugin';
+
+const createHeader = ( base: number ): string => {
+    const header = `
+================================================
+                Tabla del ${base}
+================================================
+    \n`;
+
+    return header;
+};
+
+const createTable = ( base: number, limit: number ): string => {
+    let table = '';
+
+    for (let index = 1; index <= limit; index++) {
+        table = table.concat(`${base} x ${index} = ${base*index} \n`);
+    }
+
+    return table;
+};
+
+const showTable = ( table: string ): void => {
+    console.log(table);
+};
+
+const { b: base, l: limit, s: show } = yarg;
+
+let message = createHeader(base);
+message += createTable(base, limit);
+
+if (show) showTable(message);
+
+const outputPath = `outputs`;
+
+mkdirSync(outputPath, { recursive: true });
+writeFileSync(`${outputPath}/tabla-${base}.txt`, message);
